4d8392a5db4c8d23126f0382e0d6f8157339884c
Tristan-BS <98057075+Tristan-BS@users.noreply.github.com>
2024-12-16 09:32:55 +0100

    Changelog 16.12.2024
    -> Ooopsies with Version

--- Car-Simulator/Version.h
+++ Car-Simulator/Version.h
@@ -2,8 +2,8 @@
 #pragma once
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 26
-#define VERSION_PATCH 10
+#define VERSION_MINOR 23
+#define VERSION_PATCH 13
 #define TYPE "Beta"
 
 #define STRINGIFY(x) #x
